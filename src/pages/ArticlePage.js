0285eb2f4a0eaee4bbfdcefa767fcd3bde2f3a6e
Selviraja <i.am.selviraja.p@gmail.com>
2022-12-14 08:17:30 +0530

    URL parameters with React router have been added

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,5 +1,20 @@
+import { useParams } from "react-router-dom";
+import articles from "./article-content";
+
 const ArticlePage = () => {
-  return <h1>Article Page</h1>;
+  // key of the params object is articleId and value will be whatever that has been specified in the url after ".../articles/"
+  // const params = useParams();
+  const { articleId } = useParams();
+  const article = articles.find((article) => article.name === articleId);
+
+  return (
+    <>
+      <h1>{article.name}</h1>
+      {article.content.map((paragraph) => (
+        <p>{paragraph}</p>
+      ))}
+    </>
+  );
 };
 
 export default ArticlePage;
