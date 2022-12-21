1a8fc7eb19b50eee722e087dc1853fb885e8cae8
Selviraja <i.am.selviraja.p@gmail.com>
2022-12-21 08:45:15 +0530

    Making requests with auth tokens

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -16,7 +16,11 @@ const ArticlePage = () => {
   useEffect(() => {
     // since we're not allowed to use async as the first argument to the useEffect
     const loadArticleInfo = async () => {
-      const response = await axios.get(`/api/articles/${articleId}`);
+      const token = user && (await user.getIdToken());
+      const headers = token ? { authtoken: token } : {};
+      const response = await axios.get(`/api/articles/${articleId}`, {
+        headers,
+      });
       const newArticleInfo = response.data;
       setArticleInfo(newArticleInfo);
     };
@@ -27,7 +31,13 @@ const ArticlePage = () => {
   const article = articles.find((article) => article.name === articleId);
 
   const addUpvote = async () => {
-    const response = await axios.put(`/api/articles/${articleId}/upvote`);
+    const token = user && (await user.getIdToken());
+    const headers = token ? { authtoken: token } : {};
+    const response = await axios.put(
+      `/api/articles/${articleId}/upvote`,
+      null,
+      { headers }
+    );
     const updatedArticle = response.data;
     setArticleInfo(updatedArticle);
   };
